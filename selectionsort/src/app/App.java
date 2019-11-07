cf16f2a64872ad1a8b312a7bd18a32a321b03686
Jan Nicklas <jan.nicklas@tuta.io>
2019-11-07 15:17:13 +0100

    Add files via upload

---
+++ selectionsort/src/app/App.java
@@ -0,0 +1,20 @@
+package app;
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] array = { 1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92 };
+        for (int i = 0; i < array.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[min] > array[j]) {
+                    min = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[min];
+            array[min] = temp;
+        }
+        System.out.print(Arrays.toString(array));
+    }
+}
